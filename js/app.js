8ccd5200a6267edd533c36ff3079e4f2d1afeb4b
Sulabasana <piotr.frydryszak@gmail.com>
2017-03-05 18:51:17 +0100

    Slider works, but mobile is terrible- need to be corrected

--- js/app.js
+++ js/app.js
@@ -1,50 +1,65 @@
 let plus = document.querySelector('.next')
 let minus = document.querySelector('.prev')
-
+let play = document.querySelector('.play')
+let stop = document.querySelector('.pause')
+let click = document.querySelector(".click")
 let slideIndex = 1;
 showSlides(slideIndex) 
 auto()
+
 // function currentSlide(n){
 // 	showSlides(slideIndex = n)
 // }
-function slidePlus(n){
-	showSlides(slideIndex += n )
-}
-function slideMinus(n){
-	showSlides(slideIndex -= n)
-}
-
-plus.addEventListener("click", function(){
-	slidePlus(1);
-	setTimeout(time, 3000)
-});
-minus.addEventListener("click", function(){
-	slideMinus(-1);
-})
-function showSlides(n){
-	let i = 0;
-	const slider = document.querySelectorAll(".slider img")
-	const len = slider.length
-
-	if(n>len){slideIndex = 1}
-	if(n<1){slideIndex = len}
-
-	for(i=0;i<len;i++){
-		slider[i].style.display = "none"
+	function slidePlus(n){
+		showSlides(slideIndex += n )
 	}
-	slider[slideIndex-1].style.display = "block"; //because first index is 0 
-}
-
-function auto(){
-	let i = 0;
-	const slider = document.querySelectorAll(".slider img")
-	const len = slider.length;
-	slideIndex++
-	if(slideIndex>len){slideIndex = 1};
-	for(i;i<len;i++){
-		slider[i].style.display = "none"
+	function slideMinus(n){
+		showSlides(slideIndex -= n)
 	}
-	slider[slideIndex-1].style.display = "block"
-	time = setTimeout(auto, 3000)
-	
-}
+	click.addEventListener("click",function(){
+		clearTimeout(time)
+		auto()
+	});
+	plus.addEventListener("click", function(){
+		slidePlus(1);
+	});
+	minus.addEventListener("click", function(){
+		slideMinus(-1);
+	})
+	stop.addEventListener("click", function(){
+		if(time){
+			 clearTimeout(time)
+			 console.log("stop")
+		}
+	});
+	play.addEventListener("click", function(){
+		setTimeout(auto,3000);
+		console.log("Start!")
+	})
+	function showSlides(n){
+		let i = 0;
+		const slider = document.querySelectorAll(".slider img")
+		const len = slider.length
+
+		if(n>len){slideIndex = 1}
+		if(n<1){slideIndex = len}
+
+		for(i=0;i<len;i++){
+			slider[i].style.display = "none"
+		}
+		slider[slideIndex-1].style.display = "block"; //because first index is 0 
+	}
+
+	function auto(){
+		let i = 0;
+		const slider = document.querySelectorAll(".slider img")
+		const len = slider.length;
+		slideIndex++
+		if(slideIndex>len){slideIndex = 1};
+		for(i;i<len;i++){
+			slider[i].style.display = "none"
+		}
+		slider[slideIndex-1].style.display = "block"
+		time = setTimeout(auto, 3000)
+		
+	}
